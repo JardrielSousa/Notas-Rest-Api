bdfd39e68f540daac37571a2238e97ee9067b80d
= <jardriel.redes18@gmail.com>
2019-01-13 12:58:57 -0300

    Atualização das Funcionalidades Edit / Delete

--- src/br/com/rest/main/NotasService.java
+++ src/br/com/rest/main/NotasService.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -33,6 +36,26 @@ public class NotasService {
         }
         return lista;
     }
+	 @POST
+	    @Path("/add")
+	    @Consumes(MediaType.APPLICATION_JSON)
+	    @Produces(MediaType.TEXT_PLAIN)
+	    public String addNota(Notas nota) {
+	        String msg = "";
+	 
+	        System.out.println(nota.getTitulo());
+	 
+	        try {
+	            int idGerado = notasDao.addNota(nota);
+	 
+	            msg = "nota gerada = " + String.valueOf(idGerado);
+	        } catch (Exception e) {
+	            msg = "Erro ao add a nota!";
+	            e.printStackTrace();
+	        }
+	 
+	        return msg;
+	    }
 	@GET
     @Path("/get/{id}")
     @Consumes(MediaType.TEXT_PLAIN)
@@ -46,5 +69,44 @@ public class NotasService {
         }
  
         return nota;
+    }
+	@PUT
+    @Path("/edit/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String editarNota(Notas nota, @PathParam("id") int idNota) {
+        String msg = "";
+         
+        System.out.println(nota.getTitulo());
+         
+        try {
+            notasDao.editarNota(nota, idNota);
+             
+            msg = "Nota editada com sucesso!";
+        } catch (Exception e) {
+            msg = "Erro ao editar a nota!";
+            e.printStackTrace();
+        }
+         
+        return msg;
+    }
+     
+    @DELETE
+    @Path("/delete/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String removerNota(@PathParam("id") int idNota) {
+        String msg = "";
+         
+        try {
+            notasDao.removerNota(idNota);
+             
+            msg = "Nota removida com sucesso!";
+        } catch (Exception e) {
+            msg = "Erro ao remover a nota!";
+            e.printStackTrace();
+        }
+         
+        return msg;
     }
 }
